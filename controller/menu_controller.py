835e2f34ad3499ca8a735b9b94f85491a223c1e2
isw120 <row120@hotmail.fr>
2021-05-29 22:35:14 +0200

    Updated the code

--- controller/menu_controller.py
+++ controller/menu_controller.py
@@ -1,8 +1,16 @@
 from model.Tournoi import Tournoi
 from model.Joueur import Joueur
 from datetime import date
+import datetime
 
-new_players = []
+new_players = [Joueur("Abrahamyan", "Tatev",datetime.datetime(1988, 1, 13).strftime("%d/%m/%Y"), "Female", 1),
+               Joueur( "Markos", "Jan",datetime.datetime(1985, 7, 2).strftime("%d/%m/%Y"), "Male", 2),
+               Joueur( "Badelka", "Olga",datetime.datetime(2002, 7, 8).strftime("%d/%m/%Y"),"Female", 3),
+               Joueur( "Morphy", "Paul",datetime.datetime(1837, 6, 22).strftime("%d/%m/%Y"),"Male",4),
+               Joueur( "Bellin", "Jana",datetime.datetime(1947, 12, 9).strftime("%d/%m/%Y"), "Female", 5),
+               Joueur( "Nunn", "John",datetime.datetime(1955, 4, 25).strftime("%d/%m/%Y"),"Male", 6),
+               Joueur( "Xiangzhi","Bu",datetime.datetime(1985, 12, 10).strftime("%d/%m/%Y"),"Male", 7),
+               Joueur( "Sachdev", "Tania",datetime.datetime(1986, 8, 20).strftime("%d/%m/%Y"),"Female", 8)]
 
 class MenuController:
 
@@ -20,5 +28,20 @@ class MenuController:
         family_name = input("Enter player family name :")
         date_of_birth = input("Enter player date of birth :")
         sexe = input("Enter player sexe :")
-        joureur = Joueur( family_name, name, date_of_birth, sexe,0)
+        status = True
+        while(status):
+            classement = input("Enter a classement :")
+            if classement.isalpha() or int(classement) < 0:
+                print("classement must be a positive number")
+            else:
+                status = False
+
+        joureur = Joueur( family_name, name, date_of_birth, sexe, classement)
         new_players.append(joureur)
+
+    def displayPlayersList(self):
+        if len(new_players) == 0:
+            print("There are no players registred in the app")
+        else:
+            for element in new_players:
+                print(element)
--- view/menu.py
+++ view/menu.py
@@ -7,33 +7,37 @@ class Menu:
 
     def showMenu(self):
         print("Menu :")
-        print("1) Créer un nouveau tournoi")
-        print("2) Lancer un tournoi")
-        print("3) Ajouter un joueur")
-        print("4) Générer des rapports")
-        print("5) exit")
+        print("1) Ajouter un joueur")
+        print("2) Afficher la list des joueurs")
+        print("3) Créer un nouveau tournoi")
+        print("4) Lancer un tournoi")
+        print("5) Générer des rapports")
+        print("6) exit")
         self.chooseOption()
 
     def chooseOption(self):
         while (True):
             option = input("Enter an option:")
             if (option == "1"):
-                print("You choosed option 01")
-                self.controller.createANewTournement()
-                self.showMenu()
-            if (option == "3"):
-                print("you choosed option 03")
+                print("you choosed option 01")
                 self.controller.addingNewPlayers()
                 self.showMenu()
             if (option == "2"):
                 print("you choosed option 02")
-                self.controller.startingTournemant()
+                self.controller.displayPlayersList()
+                self.showMenu()
+            if (option == "3"):
+                print("you choosed option 03")
+                self.controller.createANewTournement()
                 self.showMenu()
             if (option == "4"):
                 print("you choosed option 04")
+                self.controller.startingTournemant()
+                self.showMenu()
+            if (option == "5"):
+                print("you choosed option 05")
                 self.controller.creatReports()
                 self.showMenu()
-
             else:
-                print("You choosed to exit")
+                print("you choosed to exit")
                 sys.exit()
